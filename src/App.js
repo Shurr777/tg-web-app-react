0083f821626820d03e987d4215f8bd8180f91921
Alex <Almarkiz777@gmail.com>
2024-04-17 22:50:00 +0300

    header3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {useEffect} from "react";
 import {usetelegram} from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
 
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
 
     return (
         <div className="App">
-            Work
+            <Header />
             <button onClick={onToggleButton}>toggle</button>
         </div>
     );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from "../Button/Button";
 import {usetelegram} from "../../hooks/useTelegram";
+import './Header.css'
 
 const Header = () => {
 
